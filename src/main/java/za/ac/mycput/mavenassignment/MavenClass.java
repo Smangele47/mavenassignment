2ea04b252df68de8ab2dadb1ce92891247152668
Rusty Mabiala <rustymabiala150@gmail.com>
2021-03-27 21:17:45 +0200

    first commit in maven_assignment by Adecel

---
+++ src/main/java/za/ac/mycput/mavenassignment/MavenClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.mycput.mavenassignment;
+
+/**
+ *
+ * @author Rusty Mabiala
+ */
+public class MavenClass {
+    public static void main(String[] args) {
+        System.out.println(" remove after testing ");
+    }
+}
